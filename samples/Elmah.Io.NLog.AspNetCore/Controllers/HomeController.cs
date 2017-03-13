3c183613ad478b37a75832c9f6c95971478731de
Thomas Ardal <thomasardal@gmail.com>
2017-03-13 20:27:26 +0100

    Sample for ASP.NET Core project

---
+++ samples/Elmah.Io.NLog.AspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace Elmah.Io.NLog.AspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        Logger _log = LogManager.GetCurrentClassLogger();
+
+        public IActionResult Index()
+        {
+            _log.Info("Calling Index");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            _log.Warn("About warning");
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            try
+            {
+                var i = 0;
+                var result = 10 / i;
+            }
+            catch (Exception e)
+            {
+                _log.Error(e, "Error during contact");
+            }
+
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}

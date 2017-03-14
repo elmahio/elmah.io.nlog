f4c178d44d3f947a285ba15f0bcd473d3da8c211
damienbod <damien_bod@hotmail.com>
2017-03-14 07:12:18 +0100

    configuration sample to use user sercrets for the elmah.io API_KEY and LogID

--- samples/Elmah.Io.NLog.AspNetCore/Startup.cs
+++ samples/Elmah.Io.NLog.AspNetCore/Startup.cs
@@ -6,12 +6,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using NLog;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using Elmah.Io.NLog;
 
 namespace Elmah.Io.NLog.AspNetCore
 {
     public class Startup
     {
+		private string _elmahAppKey;
+        private string _elmahLogId;
+		
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -19,6 +27,12 @@ namespace Elmah.Io.NLog.AspNetCore
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
+				
+			if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets("AspNetCoreElmahUI-c23d2237a4-eb8832a1-452ac5");
+            }
+			
             Configuration = builder.Build();
         }
 
@@ -27,15 +41,31 @@ namespace Elmah.Io.NLog.AspNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            _elmahAppKey = Configuration["ElmahAppKey"];
+            _elmahLogId = Configuration["ElmahLogId"];
+
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc();;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory.AddNLog();
+            app.AddNLogWeb();
+
+            LogManager.Configuration.Variables["elmahAppKey"] = _elmahAppKey;
+            LogManager.Configuration.Variables["elmahLogId"] = _elmahLogId;
+
+            foreach (ElmahIoTarget target in LogManager.Configuration.AllTargets.Where(t => t is ElmahIoTarget))
+            {
+                target.ApiKey = _elmahAppKey;
+                target.LogId = _elmahLogId;
+            }
+
+            LogManager.ReconfigExistingLoggers();
 
             if (env.IsDevelopment())
             {

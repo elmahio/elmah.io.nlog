e652e2f6d879ef36b7521465b788e158489c1d82
Thomas Ardal <thomasardal@gmail.com>
2019-09-27 07:43:57 +0200

    Stable version of 3.5

--- samples/Elmah.Io.NLog.Console/Program.cs
+++ samples/Elmah.Io.NLog.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NLog;
 using System;
+using System.Collections.Generic;
 
 namespace Elmah.Io.NLog.Console
 {
@@ -20,6 +21,22 @@ namespace Elmah.Io.NLog.Console
             log.Warn("This is a warning message");
             log.Trace("This is a trace message");
             log.Info("This is info with some structured logging: {quote} from {user}", "Hasta la vista, baby", "Arnold Schwarzenegger");
+
+            log.Info("A message with {type} {hostname} {application} {user} {source} {method} {version} {url}, {statusCode}, {serverVariables}, {cookies}, {form} and {queryString}",
+                "custom type",
+                "custom hostname",
+                "custom application",
+                "custom user",
+                "custom source",
+                "custom method",
+                "custom version",
+                "custom url",
+                500,
+                new Dictionary<string, string> { { "REMOTE_ADDR", "1.1.1.1" } },
+                new Dictionary<string, string> { { "_ga", "GA1.3.1162527071.1564749318" } },
+                new Dictionary<string, string> { { "username", "Arnold" } },
+                new Dictionary<string, string> { { "id", "42" } });
+
             System.Console.ReadLine();
             LogManager.Shutdown();  // Flushes and closes all targets (recommended to call this just before application exit)
         }

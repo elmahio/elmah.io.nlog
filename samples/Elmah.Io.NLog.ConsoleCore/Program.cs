5f36edf9026befad6b752991547022295472c41c
Thomas Ardal <thomasardal@gmail.com>
2020-03-25 13:26:46 +0100

    Added .NET Core console app sample

---
+++ samples/Elmah.Io.NLog.ConsoleCore/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using NLog;
+using NLog.Extensions.Logging;
+using System;
+
+namespace Elmah.Io.NLog.ConsoleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .Build();
+
+            LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+
+            Logger logger = LogManager.GetCurrentClassLogger();
+
+            logger.Debug("This is a debug message");
+            logger.Error(new Exception(), "This is an error message");
+            logger.Fatal("This is a fatal message");
+
+            LogManager.Shutdown();
+        }
+    }
+}

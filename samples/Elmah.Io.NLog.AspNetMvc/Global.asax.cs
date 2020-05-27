9779bea6f376f0b9879dd34e0eb7ca55fdc61bf0
Thomas Ardal <thomasardal@gmail.com>
2020-05-27 21:46:18 +0200

    Example of getting hooks in ASP.NET MVC

--- samples/Elmah.Io.NLog.AspNetMvc/Global.asax.cs
+++ samples/Elmah.Io.NLog.AspNetMvc/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using NLog;
+using System.Threading;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +14,21 @@ namespace Elmah.Io.NLog.AspNetMvc
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // The following code show how to get callbacks when logging through the elmah.io NLog target.
+            var target = (ElmahIoTarget)LogManager.Configuration.FindTargetByName("elmahio");
+
+            // Get a callback on every message before logging to elmah.io. Here you can set common
+            // properties like a version number, application, user, etc.
+            target.OnMessage = msg =>
+            {
+                msg.Version = "1.0.0";
+                msg.User = Thread.CurrentPrincipal.Identity?.Name;
+            };
+            target.OnError = (msg, err) =>
+            {
+                // Error already logged to NLog's self log. But maybe you want to do something else here?
+            };
         }
     }
 }

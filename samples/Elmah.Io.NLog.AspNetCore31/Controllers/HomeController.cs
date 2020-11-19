76e78216828f30b123aa177ccb797acf1d0bec51
Thomas Ardal <thomasardal@gmail.com>
2020-11-19 14:15:44 +0100

    Migrated ASP.NET Core 2.1 sample to 3.1

---
+++ samples/Elmah.Io.NLog.AspNetCore31/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Elmah.Io.NLog.AspNetCore31.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace Elmah.Io.NLog.AspNetCore31.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // Simple information logging
+            _logger.LogInformation("Calling Index");
+
+            // Example of structured logging
+            _logger.LogWarning("Index with {method}", Request.Method);
+
+            try
+            {
+                var i = 0;
+                var result = 10 / i;
+            }
+            catch (Exception e)
+            {
+                // Example of error logging with exception details
+                _logger.LogError(e, "Error during index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

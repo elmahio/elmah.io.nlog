c1f0f1cb90853b7e07af81962c0f05aa4f098516
Thomas Ardal <thomasardal@gmail.com>
2021-11-30 15:39:43 +0100

    ASP.NET Core 6 sample

---
+++ samples/Elmah.Io.NLog.AspNetCore60/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Elmah.Io.NLog.AspNetCore60.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // Simple information logging
+            _logger.LogInformation("Calling Index");
+
+            // Example of structured logging
+            _logger.LogWarning("Index with {method}", Request.Method);
+
+            try
+            {
+                var i = 0;
+                var result = 10 / i;
+            }
+            catch (Exception e)
+            {
+                // Example of error logging with exception details
+                _logger.LogError(e, "Error during index");
+            }
+        }
+    }
+}
---
+++ samples/Elmah.Io.NLog.AspNetCore60/Program.cs
@@ -0,0 +1,51 @@
+using NLog;
+using NLog.Web;
+
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+// If you need custom settings, message callbacks, etc. use the following code to modify the elmah.io target:
+//var elmahIoTarget = (Elmah.Io.NLog.ElmahIoTarget)LogManager.Configuration.FindTargetByName("elmahio");
+//elmahIoTarget.OnMessage = (message) =>
+//{
+//    message.Version = "6.0";
+//};
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddRazorPages();
+
+    builder.Host.UseNLog();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapRazorPages();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    logger.Error(e, "Stopped program because of exception");
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    LogManager.Shutdown();
+}
